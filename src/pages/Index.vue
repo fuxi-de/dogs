e84c0ac6c2dc716bb2fea2d39c871726af3ea02f
Florian Schildwaechter <florian.schildwaechter@bauerxcel.de>
2020-03-28 20:52:54 +0100

    ssr failure fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,31 @@
 <template>
   <Layout>
     <base-section>
-      <intro-area :image="image" :isHome="true" :introText="introtext"></intro-area>
+      <intro-area
+        :image="image"
+        :isHome="true"
+        :introText="introtext"
+      ></intro-area>
     </base-section>
     <base-section>
       <info-box :content="infobox"></info-box>
     </base-section>
-    <Slide>
-      <a id="home" href="/Gesundheit">
-        <span>Gesundheit</span>
-      </a>
-      <a id="home" href="/Tine">
-        <span>Tine</span>
-      </a>
-      <a id="home" href="/Nika">
-        <span>Nika</span>
-      </a>
-      <a id="home" href="/Wuerfe">
-        <span>Würfe</span>
-      </a>
-    </Slide>
+    <ClientOnly>
+      <Slide>
+        <a id="home" href="/Gesundheit">
+          <span>Gesundheit</span>
+        </a>
+        <a id="home" href="/Tine">
+          <span>Tine</span>
+        </a>
+        <a id="home" href="/Nika">
+          <span>Nika</span>
+        </a>
+        <a id="home" href="/Wuerfe">
+          <span>Würfe</span>
+        </a>
+      </Slide>
+    </ClientOnly>
     <base-section>
       <text-area :content="paragraphs[0]"></text-area>
     </base-section>
@@ -45,23 +51,21 @@
 </template>
 
 <script>
-import { Slide } from 'vue-burger-menu'
-
-import home from '../data/home.json'
-import IntroArea from '../components/IntroArea.vue'
-import BaseSection from '../components/BaseSection.vue'
-import InfoBox from '../components/InfoBox.vue'
-import TextArea from '../components/TextArea.vue'
-import Footer from '../components/Footer.vue'
-import ParallaxImage from '../components/ParallaxImage.vue'
-import LogoShowCase from '../components/LogoShowCase.vue'
+import home from "../data/home.json";
+import IntroArea from "../components/IntroArea.vue";
+import BaseSection from "../components/BaseSection.vue";
+import InfoBox from "../components/InfoBox.vue";
+import TextArea from "../components/TextArea.vue";
+import Footer from "../components/Footer.vue";
+import ParallaxImage from "../components/ParallaxImage.vue";
+import LogoShowCase from "../components/LogoShowCase.vue";
 
 export default {
   data() {
-    return home
+    return home;
   },
   metaInfo: {
-    title: 'Kooikerhondjes von der Wiesbachaue',
+    title: "Kooikerhondjes von der Wiesbachaue"
   },
   components: {
     BaseSection,
@@ -71,9 +75,12 @@ export default {
     Footer,
     ParallaxImage,
     LogoShowCase,
-    Slide,
-  },
-}
+    Slide: () =>
+      import("vue-burger-menu")
+        .then(m => m.Slide)
+        .catch()
+  }
+};
 </script>
 
 <style>
